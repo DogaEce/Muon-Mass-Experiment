ef36971515612c8e09237828b74969cd8af22abd
DogaEce <doga.ece@outlook.fr>
2025-03-17 16:22:36 +0100

    Add files via upload

---
+++ MonteCarloSimulation/pymuon/pymuon/__init__.py
@@ -0,0 +1,35 @@
+__name__ = "pymuon"
+
+available_formulae = []
+
+from pymuon.formulae.ionization_cst_formula import IonizationCstFormula
+calc_ionization_cst = IonizationCstFormula.calc_ionization_cst
+available_formulae.append(calc_ionization_cst)
+from pymuon.formulae.lorentz_factor_formula import LorentzFactorFormula
+calc_lorentz_factor = LorentzFactorFormula.calc_lorentz_factor
+available_formulae.append(calc_lorentz_factor)
+from pymuon.formulae.max_energy_collision_formula import MaxEnergyCollisionFormula
+calc_max_energy_collision = MaxEnergyCollisionFormula.calc_max_energy_collision
+available_formulae.append(calc_max_energy_collision)
+
+available_equations = []
+from pymuon.equations.bethe_bloch_equation import BetheBlochEquation
+available_equations.append(BetheBlochEquation)
+
+available_elements = []
+from pymuon.elements.element import Element
+available_elements.append(Element)
+from pymuon.elements.custom_element import CustomElement
+available_elements.append(CustomElement)
+from pymuon.elements.compound import Compound
+available_elements.append(Compound)
+
+available_simulations = []
+from pymuon.layers.single_layer import SingleLayer
+available_simulations.append(SingleLayer)
+from pymuon.layers.multi_layers import MultiLayers
+available_simulations.append(MultiLayers)
+
+
+__all__ = (available_formulae + available_equations + available_elements
+           + available_simulations)
